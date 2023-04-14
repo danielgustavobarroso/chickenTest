bdd938a50975b9a604384cfd0113f7c9f2863f30
danielgustavobarroso <129989000+danielgustavobarroso@users.noreply.github.com>
2023-04-14 15:23:16 -0300

    Delete FarmMgmt directory

--- FarmMgmt/src/main/java/com/retooling/farm/repository/FarmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.retooling.farm.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.retooling.farm.model.Farm;
-
-public interface FarmRepository extends MongoRepository<Farm, Integer>{
-
-}
