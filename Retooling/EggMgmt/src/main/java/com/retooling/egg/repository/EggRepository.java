33fc9fce936fd4973078bcfd852defa85416d584
danielgustavobarroso <129989000+danielgustavobarroso@users.noreply.github.com>
2023-05-16 06:24:31 -0300

    Delete Retooling directory

--- Retooling/EggMgmt/src/main/java/com/retooling/egg/repository/EggRepository.java
+++
@@ -1,9 +0,0 @@
-package com.retooling.egg.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.retooling.egg.model.Egg;
-
-public interface EggRepository extends MongoRepository<Egg, Integer>{
-
-}
